25ed5e3da26ef7663155386c15bedeec475c600b
Ana Ventura <anaventura1811@gmail.com>
2022-01-28 14:46:29 -0300

    feat: cria pasta utils, implementa método de leitura de arquivos csv

---
+++ src/utils/importer_csv.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def read_and_parse_csv_file(path: str) -> list:
+    with open(path, mode="r") as csv_file:
+        reader = csv.reader(csv_file)
+        return [row for row in reader]
+
+print(read_and_parse_csv_file('data/orders_1.csv'))
+#  a, b e c
