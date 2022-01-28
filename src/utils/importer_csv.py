93351baefacaff0071c405c951d9503dac0d123d
Ana Ventura <anaventura1811@gmail.com>
2022-01-28 15:54:21 -0300

    commit pra abrir o PR do projeto, salvando linha de raciocínio no remoto

--- src/utils/importer_csv.py
+++ src/utils/importer_csv.py
@@ -6,5 +6,12 @@ def read_and_parse_csv_file(path: str) -> list:
         reader = csv.reader(csv_file)
         return [row for row in reader]
 
-# print(read_and_parse_csv_file('data/orders_1.csv'))
+# read_and_parse_csv_file('data/orders_1.csv')
+list_of_orders = read_and_parse_csv_file('data/orders_1.csv')
+clients = []
+for a, b, c in list_of_orders:
+    # print (a)
+    clients.append(a)
+# print(set(clients))
+    
 #  a, b e c
